0e869de4a71a4ba957a1d528be354bee02b8eee5
JJKania9711 <116130334+JJKania9711@users.noreply.github.com>
2022-11-22 07:52:44 -0800

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by James on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
